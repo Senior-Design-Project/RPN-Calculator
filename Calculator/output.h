270efe3c7e7f54f8e33bfcd884213a73dcc003a7
Julian Runyon <julian.runyon@gmail.com>
2019-10-10 15:59:50 -0400

    added error

--- Calculator/output.h
+++ Calculator/output.h
@@ -4,7 +4,9 @@
 void output( double (&stack)[4], LiquidCrystal_I2C &lcd ) {
   lcd.clear();
   char string[17] = "";
+  char error[] = "And I ope!";
   // Check the size of the number
+  if(!isinf(stack[0]) && !isnan(stack[0])){
   if( abs(stack[0]) >= 1000000 || abs(stack[0]) <= 0.001 ) {
     // Scientific Notation
     dtostre( stack[0], string, 7, DTOSTR_UPPERCASE | DTOSTR_ALWAYS_SIGN );
@@ -13,5 +15,6 @@ void output( double (&stack)[4], LiquidCrystal_I2C &lcd ) {
   }
   // https://www.microchip.com/webdoc/AVRLibcReferenceManual/group__avr__stdlib_1ga060c998e77fb5fc0d3168b3ce8771d42.html
   lcd.print(string);
+  } else lcd.print(error);
   return;
 }
